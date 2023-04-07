00038d5abeb4efc374f6290e32d239a9f0c053fa
Chris Watia <cwatia@seaburysolutions.com>
2023-04-07 11:18:51 +0300

    added budget items

---
+++ src/main/java/com/mycompany/personalfinance_v2/Budget.java
@@ -0,0 +1,51 @@
+package com.mycompany.personalfinance_v1;
+
+import java.io.Serializable;
+
+public class Budget implements Serializable {
+    private int id;
+    private String name;
+    private double amount;
+
+    public Budget(int id, String name, double amount) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
